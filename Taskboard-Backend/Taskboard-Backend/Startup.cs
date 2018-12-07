f0dd1a5d11a24cfa265f789b52a242ae91f09ae8
araxis <oreshko2010@gmail.com>
2018-12-07 12:52:53 +0200

    Services and authorization configuration

--- Taskboard-Backend/Taskboard-Backend/Startup.cs
+++ Taskboard-Backend/Taskboard-Backend/Startup.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using BusinessLayer.Services;
+using BusinessLayer.Services.Interfaces;
+using DomainModels.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
+using RepositoryLayer.EntityFramework;
 using RepositoryLayer.EntityFramework.Context;
+using RepositoryLayer.Repository;
+using System.Text;
 
 namespace WebApi
 {
@@ -32,6 +40,48 @@ namespace WebApi
             });
 
             services.AddDbContext<TaskboardContext>(options => options.UseSqlServer(Configuration.GetConnectionString("WorkConnection"), b => b.MigrationsAssembly("WebApi")));
+
+            ConfigureEfRepositories(services);
+            ConfigureItemServices(services);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)
+                        ),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                    };
+                });
+
+        }
+
+        public void ConfigureEfRepositories(IServiceCollection services)
+        {
+            services.AddTransient<IRepository<User>, EfRepository<User>>();
+            services.AddTransient<IRepository<Board>, EfRepository<Board>>();
+            services.AddTransient<IRepository<Task>, EfRepository<Task>>();
+            services.AddTransient<IRepository<Subtask>, EfRepository<Subtask>>();
+            services.AddTransient<IRepository<Note>, EfRepository<Note>>();
+
+            services.AddTransient<IUserRepository, EfUserRepository>();
+            services.AddTransient<IBoardRepository, EfBoardRepository>();
+            services.AddTransient<ITaskRepository, EfTaskRepository>();
+            services.AddTransient<ISubtaskRepository, EfSubtaskRepository>();
+            services.AddTransient<INoteRepository, EfNoteRepository>();
+        }
+
+        private void ConfigureItemServices(IServiceCollection services)
+        {
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IBoardService, BoardService>();
+            services.AddTransient<ITaskService, TaskService>();
+            services.AddTransient<ISubtaskService, SubtaskService>();
+            services.AddTransient<INoteService, NoteService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,9 +96,12 @@ namespace WebApi
                 app.UseHsts();
             }
 
-            app.UseCors(builder => builder.AllowAnyOrigin());
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+
             app.UseMvc();
         }
     }
