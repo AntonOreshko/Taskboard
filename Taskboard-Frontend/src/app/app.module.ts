5056c1b11c3317ddf6634cb1fcfdab1857082ecb
araxis <oreshko2010@gmail.com>
2018-12-10 17:49:42 +0200

    register reactive form

--- Taskboard-Frontend/src/app/app.module.ts
+++ Taskboard-Frontend/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Taskboard-Frontend/src/app/register/register.component.ts
+++ Taskboard-Frontend/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { RegisterData } from '../Interfaces/register-data';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'fullname': new FormControl(null, Validators.required),
+      'password': new FormControl(null, Validators.required),
+      'confirmPassword': new FormControl(null, Validators.required)
+    }, this.confirmPasswordValidator);
+  }
+
+  confirmPasswordValidator(group: FormGroup) {
+    if (group.controls['password'].value !== group.controls['confirmPassword'].value) {
+      return  {'passwordsAreNotEqual' : true };
+    }
+    return null;
+  }
+
+  register() {
+    console.log(this.registerForm);
+
+    const registerData: RegisterData = {
+      email: this.registerForm.controls['email'].value,
+      fullname: this.registerForm.controls['fullname'].value,
+      password: this.registerForm.controls['password'].value
+    };
   }
 
 }
